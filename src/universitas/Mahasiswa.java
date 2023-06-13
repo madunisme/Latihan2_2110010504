38a59a50a13848582f478f97b6c9f114530b31ad
madunisme <machabubble19@gmail.com>
2023-06-13 20:06:09 +0800

    Melengkapi Class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -5,6 +5,38 @@ package universitas;
 public class Mahasiswa {
     private String npm, nama, prodi;
     
+    public Mahasiswa(String npm, String nama, String prodi){
+        this.nama = nama;
+        this.npm = npm;
+        this.prodi = prodi;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    
+    
     public void getDetail (){
         System.out.println("NPM : " + npm);
         System.out.println("Nama : " + nama);
