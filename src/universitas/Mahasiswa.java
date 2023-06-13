638979f168243361a97266d10bb6f6d86d0baf20
madunisme <machabubble19@gmail.com>
2023-06-13 20:02:31 +0800

    Membuat Class Mahasiswa dan MahasiswaBeraksi

---
+++ src/universitas/Mahasiswa.java
@@ -0,0 +1,13 @@
+
+package universitas;
+
+
+public class Mahasiswa {
+    private String npm, nama, prodi;
+    
+    public void getDetail (){
+        System.out.println("NPM : " + npm);
+        System.out.println("Nama : " + nama);
+        System.out.println("Prodi : " + prodi);
+    }
+}
