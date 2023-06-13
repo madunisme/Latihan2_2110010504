91a2303b7860b68df5b47360bd8bb2158ed90526
madunisme <machabubble19@gmail.com>
2023-06-13 20:11:13 +0800

    Menjalankan Program

--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -4,6 +4,7 @@ package universitas;
 
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
-        
+        Mahasiswa madun = new Mahasiswa("2110010504","Muhammad Rahmadhani Dzulfian","Teknik Informatika");
+        madun.getDetail();
     }
 }
